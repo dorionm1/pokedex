cd106d16b260afe67640f77c0991aa57aeb56aaf
Dorion Mitchell <dorionmitchell@Dorions-MBP.local>
2024-01-12 07:43:41 -0500

    pokemon app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const pokemon = [
+{id: 4,   name: 'Charmander', type: 'fire',     base_experience: 62},
+{id: 7,   name: 'Squirtle',   type: 'water',    base_experience: 63},
+{id: 11,  name: 'Metapod',    type: 'bug',      base_experience: 72},
+{id: 12,  name: 'Butterfree', type: 'flying',   base_experience: 178},
+{id: 25,  name: 'Pikachu',    type: 'electric', base_experience: 112},
+{id: 39,  name: 'Jigglypuff', type: 'normal',   base_experience: 95},
+{id: 94,  name: 'Gengar',     type: 'poison',   base_experience: 225},
+{id: 133, name: 'Eevee',      type: 'normal',   base_experience: 65}
+]
+
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1 id="header">Pokedex</h1>
+  )
+}
+
+const Pokecard = (props) => {
+  return (
+    <div id="card">
+      <p id="topText">{props.name}</p>
+      <img id="image" src={props.image} alt="Pokemon"></img>
+      <p id="type">Type: {props.type}</p>
+      <p id="exp">EXP: {props.exp}</p>
+    </div>
+  );
+}
+
+const Pokedex = () => {
+  return (
+    <div id="container">
+      <ul id="pokeList">
+        { pokemon.map(p => <li><Pokecard name={p.name} image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${p.id}.png`} type={p.type} exp={p.base_experience}/></li>)}
+      </ul>
+    </div>
+  );
+}
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header/>
+      <Pokedex/>
     </div>
   );
 }
